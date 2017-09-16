c580454c581a8930bea7a9b53998a95aef3c8bce
Jave <kasonpt@163.com>
2017-09-17 03:40:37 +0800

    [透明的修改HTTP请求] Draft version, test fail.

--- go/shiyanlou/1st/README.md
+++ go/shiyanlou/1st/README.md
@@ -1,2 +1,2 @@
 # 透明的修改 HTTP 请求
-实现一个简单的数据校验功能，对请求 body 做 md5 计算，然后把结果转为十六进制添加到请求 header 中，头部名称为X-Md5。也就是在HTTP 请求 header 中添加一个 X-Md5: <hex md5 of body> 键值对，如果body为空那么就不填。
+实现一个简单的数据校验功能，对请求body做md5计算，然后把结果转为十六进制添加到请求header中，头部名称为X-Md5。也就是在HTTP 请求header中添加一个X-Md5: <hex md5 of body> 键值对，如果body为空那么就不填。
--- go/shiyanlou/1st/md5_transport.go
+++ go/shiyanlou/1st/md5_transport.go
@@ -1,20 +1,44 @@
 package md5transport
 
 import (
-    "net/http"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type Transport struct {
-    http.RoundTripper
+	http.RoundTripper
 }
 
 func NewTransport(transport http.RoundTripper) http.RoundTripper {
-    return &Transport{transport}
+	return &Transport{transport}
 }
 
-func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
+func (t *Transport) RoundTrip(r *http.Request) (*http.Response, error) {
 
-    // 补全代码
+	// 补全代码
+	if r.Body == nil {
+		return t.RoundTripper.RoundTrip(r)
+	}
+	b, err := ioutil.ReadAll(r.Body)
+	//ast.Nil(err)
+	if err != nil {
+		fmt.Println("[RoundTrip] ioutil read fail")
+	} else {
+		if len(b) == 0 {
+			fmt.Println("[RoundTrip] len == 0")
+			md5exp := ""
+			r.Header.Set("X-Md5", md5exp)
+		} else {
+			fmt.Println("[RoundTrip] len != 0")
+			hexB := md5.Sum(b)
+			md5exp := hex.EncodeToString(hexB[:])
+			r.Header.Set("X-Md5", md5exp)
+		}
 
-    return t.RoundTripper.RoundTrip(req)
+	}
+
+	return t.RoundTripper.RoundTrip(r)
 }
