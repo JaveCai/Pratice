519a31eb3bd5dca81692db557636a40914c708d4
Jave <kasonpt@163.com>
2017-09-17 03:37:39 +0800

    [json注释解释器]: finish

--- go/shiyanlou/3rd/jsonex.go
+++ go/shiyanlou/3rd/jsonex.go
@@ -22,7 +22,48 @@ func trimCommentsLine(line []byte) []byte {
 	var newLine []byte
 
 	// 补全代码
-	
+	idx := IndexComment(line)
+	if idx == -1 {
+		newLine = line
+	} else {
+		newLine = line[:idx]
+	}
 
 	return newLine
+}
+
+func IndexComment(line []byte) int {
+	idx := 0
+	for idx != -1 {
+		i := bytes.IndexAny(line[idx:], `#`)
+		if i == -1 {
+			//fmt.Printf("[IndexComment]ret -1\n")
+			return -1
+		}
+		//fmt.Printf("[IndexComment]i:%d\n", i)
+		idx = idx + i
+		if idx >= len(line) {
+			//fmt.Printf("[IndexComment]over len\n")
+			return -1
+		}
+
+		//fmt.Printf("[IndexComment]idx:%d\n", idx)
+		dqCount := bytes.Count(line[:idx], []byte(`"`))
+		str_dqCount := bytes.Count(line[:idx], []byte(`\"`))
+		// 1.remove the count of " which as part of string
+		dqCount = dqCount - str_dqCount
+		//fmt.Printf("[IndexComment]dq count:%d\n", dqCount)
+		if dqCount%2 == 0 {
+			// 2.before this #, if there are two real ", then this # match
+			//fmt.Printf("[IndexComment]ret idx:%d\n", idx)
+			//fmt.Printf("[IndexComment]%s\n", line[:idx])
+			return idx
+		} else {
+
+			// 3.find next #
+			idx++
+		}
+	}
+	//fmt.Printf("[IndexComment]ret -1\n")
+	return -1
 }
